3064f9d77f528811ea344ebb2a8efeec79eab845
doorcs <castledoor@pusan.ac.kr>
2025-02-14 04:50:41 +0900

    JWT 관련 유틸리티 함수 수정
    - 반환 타입 표시 (str, dict)
    - decode_token() 함수의 예외 처리에 대한 설명 추가
    - JSON 직렬화를 위해 datetime형식인 created_at 필드 제거
    ( TypeError: Object of type datetime is not JSON serializable 대응 )
    - JWT 토큰 생성 시, 토큰에 필요하지 않은 `deleted` 필드 제거

--- app/dependencies/jwt.py
+++ app/dependencies/jwt.py
@@ -12,16 +12,19 @@ ALGORITHM = env.algorithm
 class JWTUtil:
     def create_token(
         self, payload: dict, expires_delta: timedelta | None = timedelta(minutes=60)
-    ):
+    ) -> str:
         payload.update({"exp": datetime.now(timezone.utc) + expires_delta})
+        del payload["deleted"]
+        del payload["created_at"]
         return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
-    def decode_token(self, token: str):
+    def decode_token(self, token: str) -> dict:
         try:
-            return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        except (
-            ExpiredSignatureError
-        ):  # jwt.decode()는 기본적으로 "exp" 필드에 대한 검사를 수행!
+            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            return payload
+        # jwt.decode()는 기본적으로 "exp" 필드에 대한 검사를 수행!
+        except ExpiredSignatureError:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        # 위조/변조된 토큰일 경우 ( 토큰 signature 검증에 실패했을 경우 )
         except JWTError:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)

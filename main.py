0c4aefee4d88b7a00c4911d40794a0e2551796a0
doorcs <castledoor@pusan.ac.kr>
2025-02-14 03:35:40 +0900

    DB 생성 함수 호출방식 변경
    - db_init()을 직접 실행하는 대신, lifespan 이벤트 핸들러 활용
    - reference: https://fastapi.tiangolo.com/advanced/events/#lifespan

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.routers import auth_router
 from app.dependencies.db import db_init
 
-db_init()
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    db_init()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
 app.include_router(auth_router.router)

86d015c59132daa2590ca58134b1f5aba38eedb4
doorcs <castledoor@pusan.ac.kr>
2025-02-17 00:43:59 +0900

    v2 Initial commit
    v1 API - https://github.com/doorcs/practice-fastapi/tree/v1
    
    v2 API에서 개선해볼 것들:
    
    - pytest를 활용해 API 테스트 자동화해보기
      - 직접 눌러보는 방식의 테스트는 그만
    - 좀 더 정교한 DB 모델링을 위해 고민해보기
      - `back-propagates` 옵션을 써보자
    - 확장에 대해서는 열려있되, 당장 필요한것부터 개발하기
      - Ledger 엔티티의 income, expense 필드는 v1 API에서 결국 안 쓰였다
    - 인증 방식 개선해보기
      - OAuth2 공부해보자
    - 비동기 처리 적용해보기
      - 죄다 `async` 키워드를 붙이면 되는게 아니다

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
-
-from app.routers import auth_router
-from app.routers import ledger_router
-from app.dependencies.db import db_init
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    db_init()
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
-
-app.include_router(auth_router.router)
-app.include_router(ledger_router.router)
+app = FastAPI()
